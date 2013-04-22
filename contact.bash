090ce95c9014cf06a74a09e93f1a79571ef30b7a
scottie33 <scottie.odom@gmail.com>
2013-04-22 15:41:52 +0800

    contacts part updated

--- contact.bash
+++ contact.bash
@@ -1,13 +1,60 @@
 #!/bin/bash
 
-if [ $# -lt 8 ]; then
+if [ $# -lt 1 ]; then # 0 para
 	echo " you should have VMD installed with path set well."
 	echo " "
-	echo " cmd psffile dcdfile fromid1 toid1 fromid2 toid2 sigma1 sigma2"
+	echo " $ cmd psffile dcdfile fromid1 toid1 fromid2 toid2 sigma1 sigma2"
+	echo " - or - "
+	echo " $ cmd num_of_atoms "
+	echo " (number of atoms of current system needed, to created normalized figure of all kinds of contacts)"
+	echo " please try again."
+	exit -1
+fi
+
+if [ $# -lt 2 ]; then # 1 para
+	#### gnuplot code to be added here ####
+	if [ ! -f "contacts.dat" -o ! -s "contacts.dat" ];then
+		echo " [ contacts.dat ] does not exist, please use "
+		echo " $ cmd psffile dcdfile fromid1 toid1 fromid2 toid2 sigma1 sigma2"
+		echo " to generate it first."
+		echo " please try again later."  
+		exit -1
+	fi
+	echo " normalized data will be plotted ... "
+	echo "inp='contacts.dat'" > tempdata.gpl
+	echo "out='Normalized-Contacts'" >> tempdata.gpl
+	echo "colx=1" >> tempdata.gpl
+	echo "col2=2" >> tempdata.gpl
+	echo "col3=3" >> tempdata.gpl
+	echo "col4=4" >> tempdata.gpl
+	echo "col5=5" >> tempdata.gpl
+	echo "totnum=$1" >> tempdata.gpl
+	echo "xlabeltext='TimeStep'" >> tempdata.gpl
+	echo "ylabeltext='Contacts'" >> tempdata.gpl
+	gnuplot draw_data_contact.gpl
+	echo " check out your [ Normalized-Contacts.eps ] :)"
+	#######################################
+	exit 0
+fi 
+
+if [ $# -lt 8 ]; then # 2 - 7 paras
+	echo " you should have VMD installed with path set well."
+	echo " "
+	echo " $ cmd psffile dcdfile fromid1 toid1 fromid2 toid2 sigma1 sigma2"
+	echo " - or - "
+	echo " $ cmd num_of_atoms "
+	echo " (number of atoms of current system needed, to created normalized figure of all kinds of contacts)"
 	echo " "
 	echo " please try again."
 	exit -1
 fi
+
+if [ -f "contacts.dat" ];then
+	cp contacts.dat protected_contacts.dat
+	echo " backup [ contacts.dat ] to [ protected_contacts.dat ] "
+fi 
+
+echo " will do statistics of contacts number, loading dcd now..."
 
 #### generating tempinput.tcl here ####
 echo  "set inputpsf \"$1\"" > tempinput.tcl
@@ -32,7 +79,7 @@ for i in `echo PP PN NP NN`; do
 	echo "colx=1" >> tempdata.gpl
 	echo "coly=${num}" >> tempdata.gpl
 	echo "xlabeltext='TimeStep'" >> tempdata.gpl
-	echo "ylabeltext='Contacts_$i'" >> tempdata.gpl
+	echo "ylabeltext='Contacts-$i'" >> tempdata.gpl
 	gnuplot draw_data.gpl
 	echo " check out your [ ${i}-contact.eps ] :)"
 done
